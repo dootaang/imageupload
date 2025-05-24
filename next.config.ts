6ebee810fe58432d86388afefccab93e4fbe4007
dootaang <kksk980@gmail.com>
2025-05-24 19:22:45 +0900

    Deploy to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  basePath: '/imageupload',
+  assetPrefix: '/imageupload/',
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
